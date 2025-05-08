91ed9815e83b4269e97a5032ac8afab7c474ae77
delv <151720755+trulyursdelv@users.noreply.github.com>
2025-05-08 14:24:56 +0800

    update v1.rb

--- build/api/v1.rb
+++ build/api/v1.rb
@@ -1,1 +1,78 @@
+require "pocketdb"
+require "navykit"
+require "json"
 
+app = NavyKit.new
+Handler = Proc.new do |request, response|
+  app.use(request, response)
+end
+
+def send(response, status, payload=nil)
+  result = { success: status }
+  if success && !payload.nil?
+    result["result"] = payload
+  else
+    result["error"] = payload
+  end
+  response.send(200, result.to_json)
+end
+
+app.post("/api/v1/write") do |request, response|
+  begin
+    token = request.query["token"]
+    record = request.query["record"]
+    if record.nil?
+      next send(response, false, "INCOMPLETE_PARAMETERS")
+    end
+    pdb = PocketDatabase.new(token: token)
+    pdb.write(record, request.json)
+    next send(response, true)
+  rescue Exception => e
+    next send(response, false, e.backtrace)
+  end
+end
+
+app.get("/api/v1/read") do |request, response|
+  begin
+    token = request.query["token"]
+    record = request.query["record"]
+    if record.nil?
+      next send(response, false, "INCOMPLETE_PARAMETERS")
+    end
+    pdb = PocketDatabase.new(token: token)
+    result = pdb.read(record)
+    next send(response, true, result)
+  rescue Exception => e
+    next send(response, false, e.backtrace)
+  end
+end
+
+app.get("/api/v1/has") do |request, response|
+  begin
+    token = request.query["token"]
+    record = request.query["record"]
+    if record.nil?
+      next send(response, false, "INCOMPLETE_PARAMETERS")
+    end
+    pdb = PocketDatabase.new(token: token)
+    result = pdb.has(record)
+    next send(response, true, result)
+  rescue Exception => e
+    next send(response, false, e.backtrace)
+  end
+end
+
+app.get("/api/v1/delete") do |request, response|
+  begin
+    token = request.query["token"]
+    record = request.query["record"]
+    if record.nil?
+      next send(response, false, "INCOMPLETE_PARAMETERS")
+    end
+    pdb = PocketDatabase.new(token: token)
+    pdb.delete(record)
+    next send(response, true)
+  rescue Exception => e
+    next send(response, false, e.backtrace)
+  end
+end
